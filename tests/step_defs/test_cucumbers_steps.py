cbc921cf2da5e0f664c7bbaa48275b1cc7eaf87f
mkdv00 <maxim.cudaew@gmail.com>
2022-03-23 23:52:39 +0300

    Added new scenario and parsers.cfparse

--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,24 +1,30 @@
-from pytest_bdd import scenario, given, when, then
-import os
+from pytest_bdd import scenarios, parsers, given, when, then
 
 from ...cucumbers import CucumberBasket
 
-
-@scenario('../features/cucumbers.feature', 'Add cucumbers to a basket')
-def test_add():
-    pass
-
-
-@given('the basket has 2 cucumbers', target_fixture='basket')
-def busket():
-    return CucumberBasket(initial_count=2)
+EXTRA_TYPES: dict = {
+    'Number': int
+}
 
 
-@when("4 cucumbers are added to the basket")
-def add_cucmbers(basket: CucumberBasket):
-    basket.add(4)
+scenarios('../features/cucumbers.feature')
 
 
-@then("the basket contains 6 cucumbers")
-def basket_has_total(basket: CucumberBasket):
-    assert basket.count == 6
+@given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES), target_fixture='basket')
+def busket(initial: int):
+    return CucumberBasket(initial_count=initial)
+
+
+@when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
+def add_cucmbers(basket: CucumberBasket, some: int):
+    basket.add(some)
+
+
+@when(parsers.cfparse('"{some:Number}" cucumbers are removed from the basket', extra_types=EXTRA_TYPES))
+def remove_cucumbers(basket: CucumberBasket, some: int):
+    basket.remove(some)
+
+
+@then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
+def basket_has_total(basket: CucumberBasket, total: int):
+    assert basket.count == total
